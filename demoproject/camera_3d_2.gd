7d7d0ca773109483fd7260a660c7fa5b44d73812
InfernalWAVE <0925rpowell@gmail.com>
2024-04-22 01:04:26 -0400

    added license headers to code files

--- demoproject/camera_3d_2.gd
+++ demoproject/camera_3d_2.gd
@@ -1,3 +1,10 @@
+ # ****************************************************************
+ # * Copyright (c) 2024 Ryan Powell
+ # *
+ # * This software is released under the MIT License.
+ # * See the LICENSE file in the project root for more information.
+ # *****************************************************************
+
 extends Camera3D
 
 @export var input_action_trigger: String
